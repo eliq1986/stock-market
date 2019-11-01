5ae41d86d4b706b566b36f601eee8509e16db89d
Elijah Quesada <this_boy10@yahoo.com>
2019-10-31 18:13:02 -0700

    Initial Commit

---
+++ src/components/MarketIndexContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MarketIndexContainer = () => {
+  return (
+    <div>
+      <div>
+      DOW
+      </div>
+      <div>
+      NASDAQ
+      </div>
+    </div>
+  )
+}
+
+
+export default MarketIndexContainer;
